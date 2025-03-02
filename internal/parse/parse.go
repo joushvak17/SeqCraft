33ed2034056255777f4d30a6cc42ead0d33b3d82
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-02 13:49:06 -0500

    Add TODO statements for additional implementation of functions in flags

--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -11,6 +11,11 @@ func NewParseCmd() *cobra.Command {
 	var (
 		// Define flags for the command
 		sequenceLength bool
+
+		// TODO: Add additional flags for analyzing the sequences
+		// Primarily the GC content and the reverse complement
+		// gcContent      bool
+		// reverseComp    bool
 	)
 
 	parseCmd := &cobra.Command{
@@ -33,6 +38,18 @@ func NewParseCmd() *cobra.Command {
 					fmt.Printf("Sequence Length: %d\n", length)
 				}
 
+				// TODO: Add additional analysis for the sequences
+				// if gcContent {
+				// 	// Calculate GC content
+				// 	gc := calculateGC(record.Sequence)
+				// 	fmt.Printf("GC Content: %.2f%%\n", gc)
+				// }
+				// if reverseComp {
+				// 	// Calculate reverse complement
+				// 	reverse := reverseComplement(record.Sequence)
+				// 	fmt.Printf("Reverse Complement: %s\n", reverse)
+				// }
+
 				fmt.Println() // Add an empty line between records
 			}
 		},
@@ -40,6 +57,11 @@ func NewParseCmd() *cobra.Command {
 
 	// Add flags to the command
 	parseCmd.Flags().BoolVarP(&sequenceLength, "length", "l", false, "Calculate sequence length")
+
+	// TODO: Add additional flags for analyzing the sequences
+	// Primarily the GC content and the reverse complement
+	// parseCmd.Flags().BoolVarP(&reverseComp, "reverse", "r", false, "Calculate reverse complement")
+	// parseCmd.Flags().BoolVarP(&sequenceLength, "length", "l", false, "Calculate sequence length")
 
 	return parseCmd
 }
