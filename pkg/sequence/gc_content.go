74d26ad64fb272abb14d29fc79a6fabc04cda8a1
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-05 15:24:36 -0500

    Remove a TODO in testing file and reformatting

--- pkg/sequence/gc_content.go
+++ pkg/sequence/gc_content.go
@@ -1,6 +1,6 @@
 package sequence
 
-// GCContent calculates the GC content of a sequence
+// GCContent calculates the GC content of a sequence.
 func GCContent(seq string) float64 {
 	if len(seq) == 0 {
 		return 0.0
