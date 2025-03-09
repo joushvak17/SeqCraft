8908790f9f16676dfc57282e689756cb9b79a69c
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-09 19:54:38 -0400

    Add comments in file to specify what each part of code is doing

--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -48,13 +48,13 @@ func NewParseCmd() *cobra.Command {
 				return
 			}
 
-			// Get the current date and time
+			// Get the current date/time and create the output string
 			currentTime := time.Now().Format("January 2, 2006 3:04 PM")
-
 			output := color.GreenString("SeqCraft") + color.YellowString(" Parse Output(s):\n")
 			output += fmt.Sprintf("Date and Time: %s\n", currentTime)
 			output += fmt.Sprintf("Number of Records Parsed: %d\n", len(records))
 
+			// Create plain output string if an output file is specified
 			var plainOutput string
 			if outputFile != "" {
 				plainOutput = "SeqCraft Parse Output(s):\n"
@@ -68,27 +68,30 @@ func NewParseCmd() *cobra.Command {
 			var lengths []int
 
 			for _, record := range records {
+				// Print record information
 				output += fmt.Sprintf("\n"+color.RedString("ID:")+" %s\n", record.ID)
 				output += fmt.Sprintf(color.RedString("Description:")+" %s\n", record.Description)
 				output += fmt.Sprintf(color.RedString("Sequence:")+" %s\n\n", record.Sequence)
 
+				// Add plain output if an output file is specified
 				if outputFile != "" {
 					plainOutput += fmt.Sprintf("\nID: %s\n", record.ID)
 					plainOutput += fmt.Sprintf("Description: %s\n", record.Description)
 					plainOutput += fmt.Sprintf("Sequence: %s\n", record.Sequence)
 				}
 
+				// Sequence length
 				if sequenceLength {
 					length := len(record.Sequence)
 					totalLength += length
 					lengths = append(lengths, length)
 					output += fmt.Sprintf("Sequence Length: %d\n", length)
-
 					if outputFile != "" {
 						plainOutput += fmt.Sprintf("Sequence Length: %d\n", length)
 					}
 				}
 
+				// GC content
 				if gcContent {
 					gc := sequence.GCContent(record.Sequence)
 					totalGCContent += gc
@@ -98,8 +101,8 @@ func NewParseCmd() *cobra.Command {
 					}
 				}
 
+				// Reverse complement
 				if reverseComp {
-					// Calculate reverse complement
 					reverse := sequence.ReverseComplement(record.Sequence)
 					output += fmt.Sprintf("Reverse Complement: %s\n", reverse)
 					if outputFile != "" {
@@ -107,20 +110,18 @@ func NewParseCmd() *cobra.Command {
 					}
 				}
 
+				// Nucleotide frequency
 				if nucleotideFreq {
-					// Calculate nucleotide frequency
 					freq := sequence.NucleotideFrequency(record.Sequence)
 					for nucleotide, count := range freq {
 						totalNucleotideFreq[nucleotide] += count
 					}
 					output += "Nucleotide Frequency:\n"
-
 					if outputFile != "" {
 						plainOutput += "Nucleotide Frequency:\n"
 					}
 					for nucleotide, count := range freq {
 						output += fmt.Sprintf("%s: %.4f\n", string(nucleotide), count)
-
 						if outputFile != "" {
 							plainOutput += fmt.Sprintf("%s: %.4f\n", string(nucleotide), count)
 						}
@@ -130,11 +131,11 @@ func NewParseCmd() *cobra.Command {
 
 			// Print aggregate statistics
 			output += "\n" + color.GreenString("SeqCraft") + color.YellowString(" Aggregate Statistics:\n")
-
 			if outputFile != "" {
 				plainOutput += "\nSeqCraft Aggregate Statistics:\n"
 			}
 
+			// Sequence length
 			if sequenceLength {
 				averageLength := float64(totalLength) / float64(len(records))
 				output += fmt.Sprintf("Total Sequence Length: %d\n", totalLength)
@@ -156,6 +157,7 @@ func NewParseCmd() *cobra.Command {
 				}
 			}
 
+			// GC content
 			if gcContent {
 				averageGCContent := totalGCContent / float64(len(records))
 				output += fmt.Sprintf("Average GC Content: %.2f%%\n", averageGCContent)
@@ -165,6 +167,7 @@ func NewParseCmd() *cobra.Command {
 				}
 			}
 
+			// Nucleotide frequency
 			if nucleotideFreq {
 				output += "Total Nucleotide Frequency:\n"
 
@@ -180,8 +183,6 @@ func NewParseCmd() *cobra.Command {
 					}
 				}
 			}
-
-			// output += "\n" // Add a newline at the end
 
 			// Print or save output
 			if outputFile != "" {
@@ -207,7 +208,7 @@ func NewParseCmd() *cobra.Command {
 	return parseCmd
 }
 
-// calculateLengthStats calculates the minimum, maximum, and median lengths from a slice of lengths.
+// Calculates the minimum, maximum, and median lengths from a slice of lengths.
 func calculateLengthStats(lengths []int) (min, max int, median float64) {
 	if len(lengths) == 0 {
 		return 0, 0, 0
