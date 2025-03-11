51ab1c886a0f8ccbf8d6eb140ebec3d8f55af8d2
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-11 11:11:35 -0400

    Remove the TODO list in the root CLI file and move to seperate file, which is ignored

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -22,32 +22,3 @@ func init() {
 	// Add the parse command to the root command
 	rootCmd.AddCommand(parse.NewParseCmd())
 }
-
-// TODO: The following will list out the things that need to be done in the future
-
-// 1. Improve Usability
-// - Input validation and error messages
-// - Easier to read output formats (e.g. tabular output)
-// 	 [DONE: Added color to output for the parse command]
-// - Error handling and reporting
-// - Add more detailed help messages for each command
-// - Progress bars for long-running tasks
-// - Support for interactive mode with prompts and menus
-
-// 2. Logging
-// - Log levels: Use different log levels, like INFO, WARNING, ERROR, DEBUG
-// - Log Files: Write logs to a file for debugging and analysis
-
-// 3. Optimization
-// - Parallel Processing: Use concurrency to speed up processing
-// - Memory Optimization: Reduce memory usage for large datasets
-
-// 4. Advanced Features
-// - Sequence Alignment: Pairwise sequence alignment and support for MSA
-// - Sequence Translation: Translate DNA/RNA sequences to protein sequences
-// - Motif Search: Search for specific motifs in sequences
-// - Secondary Structure Prediction: Predict secondary structure of protein sequences
-// - Phylogenetic Analysis: Construct phylogenetic trees from sequence data
-
-// Add Support for Additional File Formats
-// Add a Web Interface
