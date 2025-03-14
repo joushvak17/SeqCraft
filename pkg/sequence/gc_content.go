448de065756bd6f506a7d82c720e6c5fdb77bb99
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-13 22:31:24 -0400

    Use byte indexing instead of range and switch statement in GC content function

--- pkg/sequence/gc_content.go
+++ pkg/sequence/gc_content.go
@@ -1,14 +1,19 @@
 package sequence
 
-// GCContent calculates the GC content of a sequence.
+// GCContent calculates the GC content of a sequence as a percentage.
 func GCContent(seq string) float64 {
+	// TODO: Consider using the bytes package when dealing with long sequences.
+	// TODO: Consider using map based cache for previously calculated GC content.
+	// TODO: Consider using goroutines to parallelize the calculation of GC content.
+
 	if len(seq) == 0 {
 		return 0.0
 	}
 
 	gcCount := 0
-	for _, base := range seq {
-		if base == 'g' || base == 'c' || base == 'G' || base == 'C' {
+	for i := range len(seq) {
+		switch seq[i] {
+		case 'g', 'c', 'G', 'C':
 			gcCount++
 		}
 	}
