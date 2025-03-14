9363fef32ffa3287b790c95a2b9f05827216306e
Joushva Kamble <kamble.joushva@gmail.com>
2025-03-14 10:36:09 -0400

    Remove TODO statements from the GC content function

--- pkg/sequence/gc_content.go
+++ pkg/sequence/gc_content.go
@@ -2,15 +2,15 @@ package sequence
 
 // GCContent calculates the GC content of a sequence as a percentage.
 func GCContent(seq string) float64 {
-	// TODO: Consider using the bytes package when dealing with long sequences.
-	// TODO: Consider using map based cache for previously calculated GC content.
 	// TODO: Consider using goroutines to parallelize the calculation of GC content.
 
 	if len(seq) == 0 {
 		return 0.0
 	}
 
-	gcCount, seqBytes := 0, []byte(seq)
+	gcCount := 0
+	seqBytes := []byte(seq)
+
 	for i := range len(seqBytes) {
 		switch seq[i] {
 		case 'g', 'c', 'G', 'C':
