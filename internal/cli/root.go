62a47d97ea8a2c1ac460732098c2ff841d2d2aaf
Joushva Kamble <kamble.joushva@gmail.com>
2025-04-30 12:16:55 -0400

    Remove color output in root cli file and change command long msg

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,15 +1,14 @@
 package cli
 
 import (
-	"github.com/fatih/color"
 	"github.com/joushvak17/SeqCraft/internal/parse"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   color.GreenString("SeqCraft"),
-	Short: color.GreenString("SeqCraft") + " - Bioinformatics CLI Tool written in Go",
-	Long:  color.GreenString("SeqCraft") + " - CLI tool for sequence analysis, alignment, and structure analysis, all accessible through an easy-to-use command line interface.",
+	Use:   "seqcraft",
+	Short: "SeqCraft - Bioinformatics CLI Tool written in Go",
+	Long:  "SeqCraft - Bioinformatics CLI tool written in Go that provides efficient features for FASTA file analysis, including sequence parsing, alignment, and structure prediction.",
 }
 
 // Execute runs the root command of the SeqCraft CLI tool.
