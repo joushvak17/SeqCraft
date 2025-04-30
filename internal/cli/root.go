00a401bacd30f21656da8d9e0a0eb340644c9d69
Joushva Kamble <kamble.joushva@gmail.com>
2025-04-30 12:00:51 -0400

    Remove unnecessary comments in root cli file

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -12,13 +12,10 @@ var rootCmd = &cobra.Command{
 	Long:  color.GreenString("SeqCraft") + " - CLI tool for sequence analysis, alignment, and structure analysis, all accessible through an easy-to-use command line interface.",
 }
 
-// Execute runs the root command and returns any error encountered.
-// It should be called to start the CLI application.
 func Execute() error {
 	return rootCmd.Execute()
 }
 
 func init() {
-	// Add the parse command to the root command
 	rootCmd.AddCommand(parse.NewParseCmd())
 }
